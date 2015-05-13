1d3f41ab4e7a1972f560e95179199281e1d07dba
Ranganath K <ranganathkomandur@gmail.com>
2015-05-13 16:12:49 +0530

    adds a function to randomly take a word and shuffle it

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,25 @@
 //console.log("Hi");
 
+var Game = {
+  arrayOfWords: ["abduction", "abolished", "adversity", "algorithm", "amplitude", "anxiously", "auctioned", "authorize", "benchmark", "bestowing", "bifurcate", "binocular", "biography", "blasphemy", "blueprint", "bothering", "boulevard", "breathing", "breakdown", "byproduct", "capturing", "cautioned", "certainly", "chemistry", "clampdown", "clergyman", "clipboard", "coastline", "cohabited", "combative", "comparing", "competing", "compliant", "comprised", "configure", "conspired", "constable", "contrived", "copyright", "countable", "cumlinate", "curtailed", "custodian", "customary", "dangerous", "defiantly", "deflation", "deformity", "deploying", "deporting", "diplomacy", "discharge", "discovery", "dislocate", "dismantle", "dragonfly", "duplicate", "education", "equitably", "exclusion", "excursion", "exhorting", "exploding", "exploring", "exporting", "expulsion"],
+  score: 0,
+  level: 1,
+  timeForLevel: 90000,
+  startGame: function() {
+    Game.startLevel;
+  }
+  startLevel: function() {
+    var arrayLength = arrayOfWords.length;
+    var index = Math.floor((Math.random()*arrayLength));
+    var word = arrayOfWords[index].split("");
+    arrayOfWords.splice(index, 1);
+    word.sort(function() {
+      return 0.5 - Math.random();
+    });
+  }
+
+}
+
 function myFunction1() {
   alert('1');
 }
