4fec1e60fe45338300c2ea61903cbbbaf7221bbd
Jayanth Rasamsetti <jaysetty@gmail.com>
2015-05-13 13:34:51 +0530

    Added Onclick cells, simple alert function

--- javascript/game.js
+++ javascript/game.js
@@ -1,1 +1,37 @@
-console.log("Hi");
+//console.log("Hi");
+
+function myFunction1() {
+  alert('1');
+}
+
+ var myFunction2 = function() {
+ 	alert('2');
+}
+
+ var myFunction3 = function() {
+ 	alert('3');
+}
+
+ var myFunction4 = function() {
+ 	alert('4');
+}
+
+ var myFunction5 = function() {
+ 	alert('5');
+}
+
+ var myFunction6 = function() {
+ 	alert('6');
+}
+
+ var myFunction7 = function() {
+ 	alert('7');
+}
+
+ var myFunction8 = function() {
+ 	alert('8');
+}
+
+ var myFunction9 = function() {
+ 	alert('9');
+}
